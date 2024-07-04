58a67aa4ae449153500c0481121507bf5c9ebfb5
Atul <atul06092001@gmail.com>
2024-07-04 12:07:38 +0530

    remove some files of admin side

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,15 +1,15 @@
-const express = require('express');
+// const express = require('express');
 
-const login = require('../middleware/AdminauthMiddleware');
-const { createCab, createTripPackage } = require('../controllers/adminController');
-const { protect } = require('../middleware/authMiddleware');
+// const login = require('../middleware/AdminauthMiddleware');
+// const { createCab, createTripPackage } = require('../controllers/adminController');
+// const { protect } = require('../middleware/authMiddleware');
 
-const router = express.Router();
+// const router = express.Router();
 
-// router.post('/users', protect, admin );
+// // router.post('/users', protect, admin );
 
-// router.post("/", login);
-router.post('/cabs', login , createCab);
-router.post('/tripPackages', login ,createTripPackage);
+// // router.post("/", login);
+// router.post('/cabs', login , createCab);
+// router.post('/tripPackages', login ,createTripPackage);
 
-module.exports = router;
+// module.exports = router;
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const { addBooking } = require('../controllers/bookingController');
-// const { protect } = require('../middleware/authMiddleware');
-const {login} = require('../middleware/AdminauthMiddleware');
 
 const router = express.Router();
 
-router.post('/', login , addBooking);
+router.post('/', addBooking);
 
 module.exports = router;
